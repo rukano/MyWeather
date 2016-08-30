7de5e9d5e27df2b0dcae0491b9edb046f55f73b1
Juan A. Romero <rukano@gmail.com>
2016-08-30 19:25:20 +0200

    TableViewController and TableViewCell created and attached to the corresponding classes. The controller creates a dummy city from WeatherData and displays it on the TableView

---
+++ My Weather/WeatherData.swift
@@ -0,0 +1,14 @@
+//
+//  WeatherData.swift
+//  My Weather
+//
+//  Created by Juan A. Romero on 30/08/16.
+//  Copyright © 2016 Juan A. Romero. All rights reserved.
+//
+
+import Foundation
+
+struct WeatherData {
+    var cityName: String
+    var currentTemperature: String
+}
---
+++ My Weather/WeatherTableViewCell.swift
@@ -0,0 +1,29 @@
+//
+//  WeatherTableViewCell.swift
+//  My Weather
+//
+//  Created by Juan A. Romero on 30/08/16.
+//  Copyright © 2016 Juan A. Romero. All rights reserved.
+//
+
+import UIKit
+
+class WeatherTableViewCell: UITableViewCell {
+    
+    // MARK: Properties
+    
+    @IBOutlet weak var cityName: UILabel!
+    @IBOutlet weak var currentTemperature: UILabel!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
