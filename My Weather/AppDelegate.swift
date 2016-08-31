66df6bfb3f5633e7d30e2aa0c87d2ea4bbba5256
Juan A. Romero <rukano@gmail.com>
2016-08-31 21:56:05 +0200

    minors spacing corrections

--- My Weather/AppDelegate.swift
+++ My Weather/AppDelegate.swift
@@ -13,8 +13,7 @@ import CoreData
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
-
-
+    
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
         return true
@@ -32,6 +31,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func applicationWillEnterForeground(application: UIApplication) {
         // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
+        
     }
 
     func applicationDidBecomeActive(application: UIApplication) {
