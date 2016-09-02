a7705b3cbea24091e0a7aa2ddcd1bcf13571758a
Juan A. Romero <rukano@gmail.com>
2016-09-02 11:54:30 +0200

    Stub for compass view class

---
+++ My Weather/WindCompassView.swift
@@ -0,0 +1,21 @@
+//
+//  WindCompassView.swift
+//  My Weather
+//
+//  Created by Juan A. Romero on 02/09/16.
+//  Copyright © 2016 Juan A. Romero. All rights reserved.
+//
+
+import UIKit
+
+class WindCompassView: UIView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
